10b58095526d0a61d1dc09dc6d8efae4890dac24
LimoYuan <yuan705791627@gmail.com>
2019-07-19 00:42:52 +0800

    修复BUG

--- scripts/main/main.sh
+++ scripts/main/main.sh
@@ -16,15 +16,17 @@ touch scripts/install.lock
 . scripts/public/public_function.sh
 . scripts/public/public_vars.sh
 
-set_proxy "$socks5_host" "$socks5_port"
-
 # TODO
 # 分别为debian和archlinux做处理
 which sudo && usermod -a -G sudo $username || { apt install sudo -y; /sbin/usermod -a -G sudo $username; }
 cmd_success $? "add username $username to sudo group"
 
 # 关闭蜂鸣音
-remove_pcspkr
+  remove_pcspkr
+# 设置代理
+  set_proxy "$socks5_host" "$socks5_port"
+# 调试, 到这里总有问题
+  Echo_color "purple" "Socks5 proxy: $socks5_ip"
 # 安装所需要的软件
 . $work_path/scripts/main/install/debian/install_software.sh
 # 安装i3-gaps
